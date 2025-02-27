71bb35925c1be071f31a9c783dbbd751bc78d422
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-02-27 16:58:29 -0300

    feat: New component 'Logo'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Category from "./components/categories/Category";
+import Logo from "./components/logo/Logo";
 import Title from "./components/title/Title";
 
 function App() {
   return (
     <>
+      <Logo />
       <Title />
       <Category title="Politics" />
       <Category title="Sport" />
---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,14 @@
+const Logo = () => {
+  return (
+    <div className="logo">
+      <div className="logo-image">
+        <img src="" alt="" />
+      </div>
+      <div className="logo-text">
+        <h1>Your Daily News</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;

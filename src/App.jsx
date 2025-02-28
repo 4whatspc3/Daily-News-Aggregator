1dc3c7126fca7b954274a2c1ab78bbff54d74cfc
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-02-28 16:48:26 -0300

    style: The navigation menu

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,15 @@ import Title from "./components/title/Title";
 function App() {
   return (
     <>
-      <Logo />
-      <Title />
-      <Category title="Politics" icon={<Icons expr={"polis"} />} />
-      <Category title="Sport" icon={<Icons expr={"sport"} />} />
-      <Category title="Cinema and TV" icon={<Icons expr={"cinema"} />} />
-      <Category title="Music" icon={<Icons expr={"music"} />} />
-      <Category title="Science" icon={<Icons expr={"science"} />} />
+      <nav>
+        <Logo />
+        <Title />
+        <Category title="Politics" icon={<Icons expr={"polis"} />} />
+        <Category title="Sport" icon={<Icons expr={"sport"} />} />
+        <Category title="Cinema and TV" icon={<Icons expr={"cinema"} />} />
+        <Category title="Music" icon={<Icons expr={"music"} />} />
+        <Category title="Science" icon={<Icons expr={"science"} />} />
+      </nav>
     </>
   );
 }

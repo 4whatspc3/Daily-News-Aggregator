31c0e689add781160be5ef8006b7fa3e62ab2fdc
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-03-02 15:05:53 -0300

    feat: New component 'News'

---
+++ src/components/news/News.jsx
@@ -0,0 +1,12 @@
+const News = ({ direction, thumbnail, content }) => {
+  return (
+    <div
+      className={
+        direction === "horizontal" ? "news horizonal" : "news vertical"
+      }
+    >
+      <div className="news-thumbnail">{thumbnail}</div>
+      <div className="new-content">{content}</div>
+    </div>
+  );
+};

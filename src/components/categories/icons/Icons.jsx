fbce859f37fe11aad838150e5485d93a948d1ff2
Matheus Augusto <69812698+4whatspc3@users.noreply.github.com>
2025-03-14 19:10:13 -0300

    fix: SVG icons not loading after deployment
    Move the 'public' folder to the correct location

--- src/components/categories/icons/Icons.jsx
+++ src/components/categories/icons/Icons.jsx
@@ -1,32 +1,23 @@
 const Icons = ({ expr }) => {
   switch (expr) {
     case "cinema":
-      return (
-        <img src="src/public/iconsCategories/cinema.svg" alt="cinema icon" />
-      );
+      return <img src="public/iconsCategories/cinema.svg" alt="cinema icon" />;
 
     case "business":
       return (
-        <img
-          src="src/public/iconsCategories/business.svg"
-          alt="business icon"
-        />
+        <img src="public/iconsCategories/business.svg" alt="business icon" />
       );
 
     case "polis":
-      return (
-        <img src="src/public/iconsCategories/polis.svg" alt="polis icon" />
-      );
+      return <img src="public/iconsCategories/polis.svg" alt="polis icon" />;
 
     case "science":
       return (
-        <img src="src/public/iconsCategories/science.svg" alt="science icon" />
+        <img src="public/iconsCategories/science.svg" alt="science icon" />
       );
 
     case "sport":
-      return (
-        <img src="src/public/iconsCategories/sport.svg" alt="sport icon" />
-      );
+      return <img src="public/iconsCategories/sport.svg" alt="sport icon" />;
     default:
       console.log("Sorry, we are out of expressions");
   }
